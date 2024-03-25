a5650b680c9925569cec44e24fd728101f1636bc
joseph517 <alvarovergara1993@hotmail.com>
2024-03-24 22:02:51 -0500

    router menu is added

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+export interface MenuItem {
+  name: string;
+  route: string;
+}
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
+
+  public MenuItem: MenuItem[] = [
+    {
+      name: 'FullScreen',
+      route: '/maps/fullscreen'
+    },
+    {
+      name: 'ZoomRange',
+      route: '/maps/zoom-range'
+    },
+    {
+      name: 'Markers',
+      route: '/maps/markers'
+    },
+    {
+      name: 'Houses',
+      route: '/maps/properties'
+    }
+  ]
 
 }
